aff662f3199ecaa54fb05d8a9a0dd7cd19259760
onmax <maximogarciamtnez@gmail.com>
2024-09-01 11:23:22 +0200

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,89 @@
+import wasm from 'vite-plugin-wasm'
+import topLevelAwait from 'vite-plugin-top-level-await'
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  future: {
+    compatibilityVersion: 4,
+  },
+
+  compatibilityDate: '2024-08-31',
+
+  ssr: false,
+  modules: [
+    '@vueuse/nuxt',
+    '@pinia/nuxt',
+    '@unocss/nuxt',
+    '@nuxtjs/color-mode',
+    '@nuxthub/core',
+    '@nuxt/eslint',
+    '@nuxt/image',
+  ],
+
+  hub: {
+    // NuxtHub options. See https://hub.nuxt.com/docs/getting-started/installation
+  },
+
+  devtools: {
+    enabled: true,
+  },
+
+  colorMode: {
+    classSuffix: '',
+  },
+
+  experimental: {
+    // when using generate, payload js assets included in sw precache manifest
+    // but missing on offline, disabling extraction it until fixed
+    payloadExtraction: false,
+    renderJsonPayloads: true,
+    typedPages: true,
+  },
+
+  vite: {
+    plugins: [
+      wasm(),
+      topLevelAwait(),
+    ],
+    optimizeDeps: {
+      exclude: ['@nimiq/core'],
+    },
+  },
+
+  nitro: {
+    esbuild: {
+      options: {
+        target: 'esnext',
+      },
+    },
+  },
+
+  app: {
+    head: {
+      viewport: 'width=device-width,initial-scale=1',
+      link: [
+        { rel: 'icon', href: '/favicon.ico', sizes: 'any' },
+        { rel: 'icon', type: 'image/icon', href: '/favicon.ico' },
+        { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
+      ],
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'description', content: 'Your Nimiq-Nuxt Template' },
+        { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
+        { name: 'theme-color', media: '(prefers-color-scheme: light)', content: 'white' },
+        { name: 'theme-color', media: '(prefers-color-scheme: dark)', content: '#1f2348' },
+      ],
+    },
+  },
+
+  features: {
+    // For UnoCSS
+    inlineStyles: false,
+  },
+
+  eslint: {
+    config: {
+      standalone: false,
+    },
+  },
+})
---
+++ unocss.config.ts
@@ -0,0 +1,19 @@
+import { presetNimiq } from 'nimiq-css'
+import { defineConfig, presetAttributify, presetUno } from 'unocss'
+import { presetRemToPx } from '@unocss/preset-rem-to-px'
+import transformerDirectives from '@unocss/transformer-directives'
+
+export default defineConfig({
+  presets: [
+    presetUno({ attributifyPseudo: true }),
+    presetNimiq({
+      utilities: true,
+      attributifyUtilities: true,
+    }),
+    presetRemToPx({ baseFontSize: 4 }),
+    presetAttributify(),
+  ],
+  transformers: [
+    transformerDirectives(),
+  ],
+})
